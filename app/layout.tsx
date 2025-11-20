933f5e426013bd67c558cd6b6a6959163e9677c0
unknown <niqodt@gmail.com>
2025-11-20 12:25:56 -0300

    google ad

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,6 +53,23 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17741877011"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17741877011');
+            `,
+          }}
+        />
         <Script
           id="organization-schema"
           type="application/ld+json"
